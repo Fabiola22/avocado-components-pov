6d6a888398809b482e084e95645da822456a2c8c
Fabiola Cabrera <viola_te@hotmail.com>
2021-01-26 16:42:52 +0800

    commented out sorting

--- src/components/taco-table/taco-table.tsx
+++ src/components/taco-table/taco-table.tsx
@@ -56,7 +56,8 @@ export class TacoTable {
           <tr>
             { this.columns.map(column =>
               <th>
-                { column.hasSorting ? <sort-column  column-name={column.title} class="red"></sort-column> : column.title }
+                {column.title}
+                {/* { column.hasSorting ? <sort-column  column-name={column.title} class="red"></sort-column> : column.title } */}
               </th>
             )}
           </tr>
